01d0b9c862cecab52cb58d5a909370eb07572665
vscalcione <vincenzo99.scalcione@gmail.com>
2020-05-29 11:21:10 +0200

    Fixing pow_recursive.c

--- recursion/pow_recursive.c
+++ recursion/pow_recursive.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int base, exponent, result;
-int pow (int base, int exponent);
+int raised_pow (int base, int exponent);
 
 int main(){
     printf("Pow recursive\n");
@@ -9,14 +9,15 @@ int main(){
     scanf("%d", &base);
     printf("Insert exponent: ");
     scanf("%d", &exponent);
-    result = pow(base, exponent);
+    result = raised_pow(base, exponent);
     printf("Pow of %d elevate at %d is: %d", base, exponent, result);
+    return 0;
 }
 
-int pow (int base, int exponent){
+int raised_pow (int base, int exponent){
     if (exponent <= 0)
         return 1;
     else
-        return base * pow(base, exponent - 1);
+        return base * raised_pow(base, exponent - 1);
 }
 
